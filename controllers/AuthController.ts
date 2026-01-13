94dbcde473fa26972b47bb1cc06b2be446ba3c46
Mufuta Mulamba Nathan <nathanmf06@gmail.com>
2026-01-13 18:55:19 +0100

    Gestion d'url d'inscription

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -14,6 +14,7 @@ export const AuthController = {
       email,
       password,
       options: {
+        emailRedirectTo: window.location.origin,
         data: {
           first_name: firstName,
           last_name: lastName,
